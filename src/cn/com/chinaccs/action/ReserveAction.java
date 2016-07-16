ddb9ad652579319f4578af50aabcb9df8c6eba24
RainerChao <zzp.doc@hotmail.com>
2016-07-16 22:19:59 +0800

    预约中心

---
+++ src/cn/com/chinaccs/action/ReserveAction.java
@@ -0,0 +1,18 @@
+package cn.com.chinaccs.action;
+
+public class ReserveAction extends BaseImplAction{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public String reserveList(){
+		return SUCCESS;
+	}
+	
+	public String hisList(){
+		return SUCCESS;
+	}
+
+}
